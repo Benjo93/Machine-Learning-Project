915b9ae74d74cd0b635e238aeab720d6b182d973
Benjamin Jordan <benjaminjordan93@gmail.com>
2021-10-21 12:09:20 -0400

    Added-App-Framework

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
 from kivy.app import App
+from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.image import Image
-from kivy.uix.label import Label
+
+
+class MainLayout(BoxLayout):
+    pass
 
 
 class MLProjectApp(App):
     def build(self):
 
-        layout = BoxLayout(padding=[10, 20, 10, 10], orientation='vertical')
+        # Change the background color.
+        Window.clearcolor = (0.9, 0.9, 0.9, 1)
 
-        btn_top = Button(text="Button top", background_color=[0.5, 0.5, 0.5, 1.0])
-        layout.add_widget(btn_top)
+        # Change window size
+        Window.size = (450, 800)
 
-        btn_bot = Button(text="Button bot", background_color=[0.8, 0.8, 0.8, 1.0])
-        layout.add_widget(btn_bot)
+        return MainLayout()
 
-        layout.add_widget(Button())
+    def on_press_button(self, function):
 
-        return layout
+        if (function == "Menu"):
+            print('Menu Button Function')
 
-    def on_press_button(self):
-        print('You pressed the button')
+        if (function == "Speak"):
+            print('Text-to-speech function')
 
+        if (function == "Read"):
+            print('Camera text extraction function')
 
 MLProjectApp().run()
