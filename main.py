6ca0f3bba61b2ae60f98d6eb175f73d9405158ac
Whiplash1199 <hank.woods12@gmail.com>
2021-11-28 00:14:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+from __future__ import print_function
+
 from kivy.app import App
-from kivy.uix.camera import Camera
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.core.window import Window
+from kivy.uix.widget import Widget
+from kivy.uix.image import Image
+from kivy.clock import Clock
+from kivy.graphics.texture import Texture
 
+#Camera Functionality
+from google.cloud import vision
+from google.cloud.vision_v1 import types
+import os
+import io
+import cv2
+credential_path = "nodal-isotope-333317-71fcbccc05ee.json"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
 
 class MainLayout(BoxLayout):
     pass
@@ -31,32 +44,46 @@ class MLProjectApp(App):
         if function == "Read":
             print('Camera text extraction function')
 
-
-MLProjectApp().run()
-
-
-# Take a picture with device camera and store it in the root directory.
-class CameraExample(App):
+class CamApp(App):
 
     def build(self):
-
-        # Get camera feed.
-        self.camera = Camera(play=True)
-        self.camera.resolution = (800, 800)
-
-        take_picture = Button(text="Take Picture")
-        take_picture.size_hint = (0.5, 0.2)
-        take_picture.pos_hint = {'x': 0.25, 'y': 0.25}
-        take_picture.bind(on_press=self.take_picture)
-
+        self.img1=Image()
         layout = BoxLayout()
-        layout.add_widget(self.camera)
-        layout.add_widget(take_picture)
+        layout.add_widget(self.img1)
+        #opencv2 stuffs
+        self.capture = cv2.VideoCapture(0)
+        cv2.namedWindow("CV2 Image")
+        Clock.schedule_interval(self.update, 1.0/33.0)
         return layout
 
-    def take_picture(self, *args):
-        print("Picture Taken")
-        self.camera.export_to_png("./camera_test.png")
+    def update(self, dt):
+        # display image from cam in opencv window
+        ret, frame = self.capture.read()
+        cv2.imshow("CV2 Image", frame)
+        # convert it to texture
+        buf1 = cv2.flip(frame, 0)
+        buf = buf1.tostring()
+        texture1 = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr') 
+        #if working on RASPBERRY PI, use colorfmt='rgba' here instead, but stick with "bgr" in blit_buffer. 
+        texture1.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+        # display image from the texture
+        self.img1.texture = texture1
 
+client = vision.ImageAnnotatorClient()
+def detect_text(path):
+    """Detects text in the file."""
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
 
-# CameraExample().run()
+    image = types.Image(content=content)
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    string = ''
+
+    for text in texts:
+        string+=' ' + text.description
+    return string
+
+if __name__ == '__main__':
+    CamApp().run()
+    cv2.destroyAllWindows()
