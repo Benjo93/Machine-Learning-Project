b081a55195a25e7735b0ce7f2de29447fea66ab6
Benjamin Jordan <benjaminjordan93@gmail.com>
2021-11-29 21:58:58 -0500

    Camera-Button
    Added camera button and made text wrap.

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from __future__ import print_function
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
 from kivy.core.window import Window
-from kivy.uix.widget import Widget
-from kivy.uix.image import Image
 from kivy.clock import Clock
 from kivy.graphics.texture import Texture
 
-#Camera Functionality
+# Import text-to-speech
+from gtts import gTTS
+
+# Camera Functionality
 from google.cloud import vision
 from google.cloud.vision_v1 import types
 import os
@@ -21,6 +21,7 @@ import cv2
 
 credential_path = "nodal-isotope-333317-71fcbccc05ee.json"
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
+
 
 class MainLayout(BoxLayout):
     pass
@@ -35,12 +36,12 @@ class MLProjectApp(App):
         # Change window size.
         Window.size = (450, 800)
 
-        #Google Vision
+        # Google Vision
         self.client = vision.ImageAnnotatorClient()
         self.detectedText = ''
-        #opencv2
+        # opencv2
         self.capture = cv2.VideoCapture(0)
-        Clock.schedule_interval(self.update, 1.0/33.0)
+        Clock.schedule_interval(self.update, 1.0 / 33.0)
 
         return MainLayout()
 
@@ -51,6 +52,7 @@ class MLProjectApp(App):
 
         if function == "Speak":
             print('Text-to-speech function')
+            self.text_to_speech()
 
         if function == "Read":
             print('Camera text extraction function')
@@ -61,18 +63,18 @@ class MLProjectApp(App):
         ret, frame = self.capture.read()
 
         file = 'images/live.png'
-        cv2.imwrite( file,frame)
-        
+        cv2.imwrite(file, frame)
+
         # convert it to texture
         buf1 = cv2.flip(frame, 0)
         buf = buf1.tostring()
-        texture1 = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr') 
+        texture1 = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
         texture1.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
         # display image from the texture
         self.root.ids.img1.texture = texture1
 
     def detect_text(self, path):
-        #Detects text in the file.
+        # Detects text in the file.
         with io.open(path, 'rb') as image_file:
             content = image_file.read()
 
@@ -82,9 +84,16 @@ class MLProjectApp(App):
         string = ''
 
         for text in texts:
-            string+=' ' + text.description
+            string += ' ' + text.description
         print(string)
         return string
+
+    def text_to_speech(self):
+        text_to_read = self.detectedText
+        text_result = gTTS(text=text_to_read, lang='en', slow=False)
+        text_result.save("text_result.mp3")
+        os.system("start text_result.mp3")
+
 
 if __name__ == '__main__':
     MLProjectApp().run()
